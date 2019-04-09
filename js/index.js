7a3844ea9775ee8b79cd3410a1bc04b68890bc38
victorN <victornguyen082@gmail.com>
2019-04-09 16:17:51 -0400

    Add on to 3rd button events + Chris and Skyler's work

--- js/index.js
+++ js/index.js
@@ -14,7 +14,21 @@ button2.addEventListener('dblclick', (event) => {
 let button3 = document.querySelectorAll('.destination .btn')[2]
 button3.addEventListener('mouseenter', (event) => {
     button3.style.background = 'green'
+    button3.innerHTML = 'Green Version'
   })
   button3.addEventListener('mouseleave', (event) => {
     button3.style.background = '#17A2B8'
+    button3.innerHTML = 'Sign Me Up!'
   })
+
+let logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+    event.target.style.cursor = 'pointer';
+})
+logoHeading.addEventListener('mouseout', (event)=> {
+    event.target.style.color = 'black';
+})
+logoHeading.addEventListener('click', (event) => {
+    alert('Thank for Visiting Fun Bus! Subscribe for Updates!');
+})
